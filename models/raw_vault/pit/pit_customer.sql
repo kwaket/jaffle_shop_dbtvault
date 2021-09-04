a9ed2a9a6b51042912116b81a8227e63aa55bc34
Savva <kwaket@live.ru>
2021-09-05 02:29:02 +0300

    rewrite pit_customer

--- models/raw_vault/pit/pit_customer.sql
+++ models/raw_vault/pit/pit_customer.sql
@@ -1,24 +1,27 @@
-with dates_customer_combinations as (
-    select customer_pk, current_date - 7 as load_date
-    from {{ ref('hub_customer') }}),
+with all_load_dates as (
+    select load_date::date from {{ ref('hub_customer') }}
+    union
+    select load_date::date from {{ ref('sat_customer_details') }}
+    union
+    select load_date::date from {{ ref('sat_customer_crm_data') }}),
+dates_customer_combinations as (
+    select c.customer_pk, d.load_date
+    from all_load_dates d
+        cross join {{ ref('hub_customer') }} as c),
 sat_details as (
     select customer_pk, load_date, first_name, last_name, email
     from {{ ref('sat_customer_details') }}),
 sat_crm as (
     select customer_pk, load_date, country, age
-    from {{ ref('sat_customer_crm_data')}}),
-pit_raw as (
-    select
-        dc.customer_pk, dc.load_date,
-        max(sd.load_date) as load_date_details,
-        max(ss.load_date) as load_date_crm_data
-    from dates_customer_combinations dc
-    join sat_details sd
-        on dc.customer_pk = sd.customer_pk
-    join sat_crm as ss -- not Schutzstaffeln
-        on dc.customer_pk = ss.customer_pk
-    group by 1, 2
-)
+    from {{ ref('sat_customer_crm_data') }})
+select
+    dc.customer_pk
+    , dc.load_date load_date
 
-select customer_pk, load_date, load_date_details, load_date_crm_data
-from pit_raw
+    , max(sd.load_date) over (partition by dc.customer_pk order by dc.load_date) as load_date_details
+    , max(sc.load_date) over (partition by dc.customer_pk order by dc.load_date) as load_date_crm_data
+from dates_customer_combinations dc
+left join sat_details sd
+    on dc.customer_pk = sd.customer_pk and dc.load_date = sd.load_date::date
+left join sat_crm sc
+    on dc.customer_pk = sc.customer_pk and dc.load_date = sc.load_date::date
